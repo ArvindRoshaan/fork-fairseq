3c0221e2a37b24273b738ed1a9f7f83a4865f225
Arvind Roshaan <91244663+ArvindRoshaan@users.noreply.github.com>
2024-03-07 16:48:56 +0530

    Update extract_last_checkpoint_predictions.sh

--- extract_last_checkpoint_predictions.sh
+++ extract_last_checkpoint_predictions.sh
@@ -3,26 +3,26 @@
 
 if [ $# -ne 14 ]; then
     echo "Mismatch in # of arguments provided"
-    echo "Please provide -n model_unique_identifier_name ; -d model_unique_description; -s source_lang; -t target_lang; -is_train 1/0; -is_valid 1/0; -is_test 1/0;"
+    echo "Please provide -n model_unique_identifier_name ; -d model_unique_description; -s source_lang; -t target_lang; -a 1/0; -b 1/0; -c 1/0;"
     exit 1
 fi
 
-while getopts n:d:s:t:is_train:is_valid:is_test: flag
+while getopts n:d:s:t:a:b:c: flag
 do
     case "${flag}" in
         n) model_folder_name=${OPTARG};;
         d) model_desc=${OPTARG};;
-	    s) source_lang=${OPTARG};;
-	    t) target_lang=${OPTARG};;
-        is_train) is_train=${OPTARG};;
-        is_valid) is_valid=${OPTARG};;
-        is_test) is_test=${OPTARG};;
+	s) source_lang=${OPTARG};;
+	t) target_lang=${OPTARG};;
+        a) is_train=${OPTARG};;
+        b) is_valid=${OPTARG};;
+        c) is_test=${OPTARG};;
     esac
 done
 
 lang=$source_lang-$target_lang
 
-if [ $is_train -ne 1 ]; then
+if [[ "$is_train" == 1 ]]; then
     echo "Extracting training text"
     grep "H-" output_custom_fldr/$model_folder_name/$model_desc/output_train_lastCP_$lang.txt | cut -f 3 > output_custom_fldr/$model_folder_name/$model_desc/output_train_lastCP_pred.txt
     grep "T-" output_custom_fldr/$model_folder_name/$model_desc/output_train_lastCP_$lang.txt | cut -f 2 > output_custom_fldr/$model_folder_name/$model_desc/output_train_lastCP_truth.txt
@@ -32,7 +32,7 @@ if [ $is_train -ne 1 ]; then
     wc -l output_custom_fldr/$model_folder_name/$model_desc/output_train_lastCP_truth.txt
 fi
 
-if [ $is_valid -ne 1 ]; then
+if [[ "$is_valid" == 1 ]]; then
     echo "Extracting validation text"
     grep "H-" output_custom_fldr/$model_folder_name/$model_desc/output_valid_lastCP_$lang.txt | cut -f 3 > output_custom_fldr/$model_folder_name/$model_desc/output_valid_lastCP_pred.txt
     grep "T-" output_custom_fldr/$model_folder_name/$model_desc/output_valid_lastCP_$lang.txt | cut -f 2 > output_custom_fldr/$model_folder_name/$model_desc/output_valid_lastCP_truth.txt
@@ -42,7 +42,7 @@ if [ $is_valid -ne 1 ]; then
     wc -l output_custom_fldr/$model_folder_name/$model_desc/output_valid_lastCP_truth.txt
 fi
 
-if [ $is_test -ne 1 ]; then
+if [[ "$is_test" == 1 ]]; then
     echo "Extracting test text"
     grep "H-" output_custom_fldr/$model_folder_name/$model_desc/output_test_lastCP_$lang.txt | cut -f 3 > output_custom_fldr/$model_folder_name/$model_desc/output_test_lastCP_pred.txt
     grep "T-" output_custom_fldr/$model_folder_name/$model_desc/output_test_lastCP_$lang.txt | cut -f 2 > output_custom_fldr/$model_folder_name/$model_desc/output_test_lastCP_truth.txt
