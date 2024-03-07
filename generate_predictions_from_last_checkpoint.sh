1306a04f0539c0f2ea3b3b9594753301563896ba
Arvind Roshaan <91244663+ArvindRoshaan@users.noreply.github.com>
2024-03-07 16:26:49 +0530

    Update generate_predictions_from_last_checkpoint.sh

--- generate_predictions_from_last_checkpoint.sh
+++ generate_predictions_from_last_checkpoint.sh
@@ -3,20 +3,20 @@
 
 if [ $# -ne 14 ]; then
     echo "Mismatch in # of arguments provided"
-    echo "Please provide -n model_unique_identifier_name ; -d model_unique_description; -s source_lang; -t target_lang; -is_train 1/0; -is_valid 1/0; -is_test 1/0;"
+    echo "Please provide -n model_unique_identifier_name ; -d model_unique_description; -s source_lang; -t target_lang; -a 1/0; -b 1/0; -c 1/0;"
     exit 1
 fi
 
-while getopts n:d:s:t:is_train:is_valid:is_test: flag
+while getopts n:d:s:t:a:b:c: flag
 do
     case "${flag}" in
         n) model_folder_name=${OPTARG};;
         d) model_desc=${OPTARG};;
-	    s) source_lang=${OPTARG};;
-	    t) target_lang=${OPTARG};;
-        is_train) is_train=${OPTARG};;
-        is_valid) is_valid=${OPTARG};;
-        is_test) is_test=${OPTARG};;
+	s) source_lang=${OPTARG};;
+	t) target_lang=${OPTARG};;
+        a) is_train=${OPTARG};;
+        b) is_valid=${OPTARG};;
+        c) is_test=${OPTARG};;
     esac
 done
 
@@ -26,13 +26,13 @@ lang=$source_lang-$target_lang
 
 echo "This code generates outputs for Training, Validation and Testing datasets for $model_folder_name, $model_desc..."
 
-if [ $is_test -ne 1 ]; then
+if [[ "$is_test" == 1 ]]; then
     echo "Generating output for testing data in ./output_custom_fldr/$model_folder_name/$model_desc"
     #Generate output
     fairseq-generate data-bin/$model_folder_name --path checkpoints/$model_folder_name/$model_desc/checkpoint_last.pt --batch-size 128 --beam 5 --remove-bpe --skip-invalid-size-inputs-valid-test > output_custom_fldr/$model_folder_name/$model_desc/output_test_lastCP_$lang.txt
 fi
 
-if [ $is_train -ne 1 ]; then
+if [[ "$is_train" == 1 ]]; then
     echo "Preparing files to generate output for training data"
     #Rename training files as test files
     cp ./data-bin/$model_folder_name/train.$lang.$src.bin ./data-bin/$model_folder_name/test.$lang.$src.bin
@@ -50,7 +50,7 @@ if [ $is_train -ne 1 ]; then
     cp ./data-bin/$model_folder_name/bkp_custom/test.$lang.$tgt.idx ./data-bin/$model_folder_name/test.$lang.$tgt.idx
 fi
 
-if [ $is_valid -ne 1 ]; then
+if [[ "$is_valid" == 1 ]]; then
     echo "Preparing files to generate output for validation data"
     #Rename validation files as test files
     cp ./data-bin/$model_folder_name/valid.$lang.$src.bin ./data-bin/$model_folder_name/test.$lang.$src.bin
